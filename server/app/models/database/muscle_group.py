8d3cb7a8664661e16b8f12b355b8bd94d03903b8
aditya-arcot <dev@aarcot.com>
2026-03-11 16:00:03 -0500

    server - add updated at column

--- server/app/models/database/muscle_group.py
+++ server/app/models/database/muscle_group.py
@@ -17,3 +17,9 @@ class MuscleGroup(Base):
         server_default=func.now(),
         nullable=False,
     )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
