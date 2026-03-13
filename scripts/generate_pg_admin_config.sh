bbbf010696e40f7a3e45d7fdb53ae12e890b0ae7
aditya-arcot <dev@aarcot.com>
2026-03-13 02:42:34 -0500

    use permanent location for generated pg admin files

--- scripts/generate_pg_admin_config.sh
+++ scripts/generate_pg_admin_config.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 set -Eeuo pipefail
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <ENV>"
+    exit 1
+fi
+ENV="$1"
+
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 BASE_DIR="$SCRIPT_DIR/.."
 ENV_DIR="$BASE_DIR/config/env"
+CONFIG_DIR="$HOME/.local/share/reptrack-$ENV"
+
 ENV_FILE="$ENV_DIR/.env"
-SERVERS_FILE="$ENV_DIR/servers.json"
-PGPASS_FILE="$ENV_DIR/pgpass"
+SERVERS_FILE="servers.json"
+PGPASS_FILE="pgpass"
+SERVERS_TEMPLATE="$ENV_DIR/$SERVERS_FILE.template"
+PGPASS_TEMPLATE="$ENV_DIR/$PGPASS_FILE.template"
+
+mkdir -p "$CONFIG_DIR"
 
 set -a
 source "$ENV_FILE"
 set +a
 
-envsubst < "$SERVERS_FILE".template > "$SERVERS_FILE"
-envsubst < "$PGPASS_FILE".template > "$PGPASS_FILE"
-chmod 600 "$PGPASS_FILE"
+envsubst < "$SERVERS_TEMPLATE" > "$CONFIG_DIR/$SERVERS_FILE"
+envsubst < "$PGPASS_TEMPLATE" > "$CONFIG_DIR/$PGPASS_FILE"
+chmod 600 "$CONFIG_DIR/$PGPASS_FILE"
+
+# symlinks for convenience
+ln -sf "$CONFIG_DIR/$SERVERS_FILE" "$ENV_DIR/$SERVERS_FILE"
+ln -sf "$CONFIG_DIR/$PGPASS_FILE" "$ENV_DIR/$PGPASS_FILE"
