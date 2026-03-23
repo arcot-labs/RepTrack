79bf74a0b51d01b1f993cb0199abdb9f54786d96
aditya-arcot <dev@aarcot.com>
2026-03-22 23:32:54 -0500

    server - clean up db models

--- server/app/models/database/feedback.py
+++ server/app/models/database/feedback.py
@@ -7,9 +7,7 @@ from sqlalchemy import (
     Index,
     func,
 )
-from sqlalchemy import (
-    Enum as SQLEnum,
-)
+from sqlalchemy import Enum as SQLEnum
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.database import Base
@@ -29,9 +27,8 @@ class Feedback(Base):
         primary_key=True,
         autoincrement=True,
     )
-    user_id: Mapped[int] = mapped_column(
+    user_id: Mapped[int | None] = mapped_column(
         ForeignKey("users.id", ondelete="SET NULL"),
-        nullable=True,
     )
     type: Mapped[FeedbackType] = mapped_column(
         SQLEnum(
