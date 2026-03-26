ca0a2615913904ee3cf576dba064faa0c09772d1
aditya-arcot <dev@aarcot.com>
2026-03-26 10:07:03 -0500

    server - fix var name

--- server/app/tests/services/_utilities/serializers/test_to_muscle_group_public.py
+++ server/app/tests/services/_utilities/serializers/test_to_muscle_group_public.py
@@ -4,20 +4,20 @@ from app.services.utilities.serializers import to_muscle_group_public
 
 
 def test_to_muscle_group_public() -> None:
-    user = MuscleGroup(
+    mg = MuscleGroup(
         id=1,
         name="Chest",
         description="Chest muscles",
     )
 
-    result = to_muscle_group_public(user)
+    result = to_muscle_group_public(mg)
 
     assert isinstance(result, MuscleGroupPublic)
     assert result.id == 1
     assert result.name == "Chest"
     assert result.description == "Chest muscles"
 
-    result = to_muscle_group_public(user)
+    result = to_muscle_group_public(mg)
 
     assert isinstance(result, MuscleGroupPublic)
     assert result.id == 1
