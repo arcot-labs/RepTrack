a083f8e97fe481885ee4afcb0bda6fc0a05b18ce
aditya-arcot <dev@aarcot.com>
2026-03-27 13:45:50 -0500

    client - update dashboard layout

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,9 +4,9 @@ export function Dashboard() {
     const { user } = useSession()
 
     return (
-        <div className="space-y-4">
+        <div className="space-y-2 max-md:p-2 md:space-y-4">
             <h1 className="text-xl font-bold">Dashboard</h1>
-            <p>Welcome {user?.first_name}</p>
+            <p>Welcome, {user?.first_name}!</p>
         </div>
     )
 }
