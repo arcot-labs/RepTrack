9fbd35a94a51ac6ff75e618cb9475c8b597451b5
aditya-arcot <dev@aarcot.com>
2026-04-09 07:37:10 -0500

    client - improve docs layout

--- client/src/components/docs/Mermaid.tsx
+++ client/src/components/docs/Mermaid.tsx
@@ -11,10 +11,24 @@ export function Mermaid({ code }: { code: string }) {
     const id = useId()
 
     useEffect(() => {
-        void mermaid.render(id, code).then(({ svg }) => {
-            if (ref.current) ref.current.innerHTML = svg
-        })
+        let isMounted = true
+        void mermaid
+            .render(id, code.trim())
+            .then(({ svg }) => {
+                if (isMounted && ref.current) ref.current.innerHTML = svg
+            })
+            .catch(() => {
+                if (isMounted && ref.current) ref.current.innerHTML = ''
+            })
+        return () => {
+            isMounted = false
+        }
     }, [code, id])
 
-    return <div ref={ref} className="my-4 flex justify-center" />
+    return (
+        <div
+            ref={ref}
+            className="my-4 flex justify-center overflow-x-auto rounded-md bg-black p-3"
+        />
+    )
 }
